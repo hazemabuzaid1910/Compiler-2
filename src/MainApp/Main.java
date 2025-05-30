deea2f9ef4149705facf83431e355d70026a8cb1
Farojeh <areejmh2003@gmail.com>
2025-05-30 15:12:54 +0300

    Resolve conflict in Main.java after stash pop

--- src/MainApp/Main.java
+++ src/MainApp/Main.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import static org.antlr.v4.runtime.CharStreams.fromFileName;
@@ -19,11 +20,9 @@ import static org.antlr.v4.runtime.CharStreams.fromFileName;
 
 public class Main {
         public static check_Symantic_Error semanticError = new check_Symantic_Error();
-        public static List<String> test = new ArrayList<>();
-        public static List<String> testcheck = new ArrayList<>();
         public static int id_element = 0 ;
         public static void main(String[] args) throws IOException {
-                File folder = new File("C://Users//Touqa2003//Desktop//Compiler-2-AST//Compiler-2-AST//Files");
+                File folder = new File("C:/Users/LENOVO/Desktop/compiler/compiler20/Compiler-2/Files");
                 File[] files = folder.listFiles();
             for (File file : Objects.requireNonNull(files)) {
                 String source = file.getPath();
@@ -34,31 +33,15 @@ public class Main {
                 ParseTree tree = parser.program();
                 Program program = (Program) new visitor().visit(tree);
                 semanticError.check_Errors();
-                for (int k =0 ; k< semanticError.getE4().getSavemap().size();k++){
-                    System.out.print(semanticError.getE4().getSavemap().get(k).getName()+"  ");
-                    System.out.print(semanticError.getE4().getSavemap().get(k).getType()+"  ");
-                    System.out.print(semanticError.getE4().getSavemap().get(k).printatt());
-                    System.out.println();
-
-                }
+                Map<String,String> map = semanticError.getE6().getCheck_map().getMap();
+                map.forEach((key, value) -> {
+                    System.out.println(key + " => " + value);
+                });
                 System.out.println();
-//                semanticError.getE4().getForequal().forEach((key, value) -> {
-//                    System.out.println(key + " = " + value);
-//                });
-
-                for (int i = 0; i < semanticError.getE4().getCheck().size(); i++) {
-                    System.out.println(
-                            semanticError.getE4().getCheck().get(i).getName()+" "+
-                            semanticError.getE4().getCheck().get(i).getValue()+" "+
-                            semanticError.getE4().getCheck().get(i).getLine());
-                }
-                System.out.println();
-                for (int i = 0; i < semanticError.getE5().getFor_list().size(); i++) {
-                    System.out.println(  semanticError.getE5().getFor_list().get(i).getParent()+" "+
-                            semanticError.getE5().getFor_list().get(i).getName()+" "+
-                                    semanticError.getE5().getFor_list().get(i).getValue()+" "+
-                                    semanticError.getE5().getFor_list().get(i).getLine());
-                }
+                Map<String,String> map1 = semanticError.getE6().get_Save_map().getMap();
+                map1.forEach((key, value) -> {
+                    System.out.println(key + " => " + value);
+                });
                 System.out.println(program);
 
             }
